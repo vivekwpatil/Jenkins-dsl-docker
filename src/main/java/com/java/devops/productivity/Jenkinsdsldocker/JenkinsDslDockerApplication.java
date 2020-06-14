67981ade7d4a6f4c68d6b07515d667ae6f624e90
Vivek Patil <patil.vivek@ing.com>
2020-06-14 23:41:09 +0200

    first commit

---
+++ src/main/java/com/java/devops/productivity/Jenkinsdsldocker/JenkinsDslDockerApplication.java
@@ -0,0 +1,13 @@
+package com.java.devops.productivity.Jenkinsdsldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDslDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDslDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/devops/productivity/Jenkinsdsldocker/JenkinsDslDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.devops.productivity.Jenkinsdsldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDslDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
